d947e32d447ef147b75ca62401cbe608e445a792
OmotolaniGbodo <olusanyaabayomi@yahoo.com>
2023-11-18 01:31:01 +0100

    Frontend Project for Top10BusLines
    Frontend Project for Top10BusLines

---
+++ src/components/BusStopList.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './BusStopList.css';
+
+const LineStops = ({ lineNumber, stops }) => (
+  <div className="bus-stop-container">
+    <h3 className="line-number">Line {lineNumber}</h3>
+    <ul className="stop-list">
+      {stops.map((stopPoint, index) => (
+        <li key={index} className="stop-item">
+          <strong>{stopPoint.StopPointName}</strong> - {stopPoint.ZoneShortName}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+const BusStopList = ({ topBusLines }) => {
+  const [busStops, setBusStops] = useState([]);
+
+  useEffect(() => {
+    const fetchBusStops = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/buslines/all?topBusLines=${topBusLines}`);
+
+        setBusStops(response.data);
+      } catch (error) {
+        console.error('Error fetching bus stops:', error);
+      }
+    };
+
+    fetchBusStops();
+  }, [topBusLines]);
+
+  return (
+    <div>
+      <h2>Bus Stops for Top {topBusLines} Bus Lines</h2>
+      {Object.keys(busStops).map((lineNumber) => (
+        <LineStops key={lineNumber} lineNumber={lineNumber} stops={busStops[lineNumber]} />
+      ))}
+    </div>
+  );
+};
+
+export default BusStopList;
